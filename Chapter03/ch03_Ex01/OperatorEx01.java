407b068d2781f6ad29ac92342a488a845623e9ae
DESKTOP-Q00AR0U\LENOVO <yeosepchoi@naver.com>
2021-10-21 15:48:39 +0900

    챕터3 1~7 코드 작성

---
+++ Chapter03/ch03_Ex01/OperatorEx01.java
@@ -0,0 +1,15 @@
+package ch03_Ex01;
+
+public class OperatorEx01 {
+	public static void main(String[] args) {
+		int i =5;
+		i++;
+		
+		System.out.println(i);
+		
+		i=5;
+		++i;
+		
+		System.out.println(i);
+	}
+}
---
+++ Chapter03/ch03_Ex05/OperatorEx05.java
@@ -0,0 +1,21 @@
+package ch03_Ex05;
+
+public class OperatorEx05 {
+	public static void main(String[] args) {
+			
+		int a = 10;
+		
+		int b = 4;
+		
+		System.out.printf("%d + %d = %d %n", a,b,a+b);
+		System.out.printf("%d - %d = %d %n", a,b,a-b);
+		System.out.printf("%d * %d = %d %n", a,b,a*b);
+		System.out.printf("%d / %d = %d %n", a,b,a/b);
+		System.out.printf("%d / %f = %f %n", a,(float)b,a/(float)b);
+		
+		
+		float c = 1234567;
+		
+		System.out.println(c);		
+	}
+}
