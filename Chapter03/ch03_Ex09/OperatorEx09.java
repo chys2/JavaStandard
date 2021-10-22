eee1150c45f91b1c270e199e1097c8a34b2cac41
DESKTOP-Q00AR0U\LENOVO <yeosepchoi@naver.com>
2021-10-22 14:33:53 +0900

    챕터3 8~14 코드 작성

---
+++ Chapter03/ch03_Ex09/OperatorEx09.java
@@ -0,0 +1,13 @@
+package ch03_Ex09;
+
+public class OperatorEx09 {
+	public static void main(String[] args) {
+		
+		long a = 1_000_000 * 1_000_000;
+		long b = 2_000_000 * 1_000_000L;
+		
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+		
+	}
+}
