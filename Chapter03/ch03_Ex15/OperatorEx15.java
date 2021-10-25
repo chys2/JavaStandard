b0e10f11dae3bfd6dd0c1243cc28dce1c89496fc
DESKTOP-Q00AR0U\LENOVO <yeosepchoi@naver.com>
2021-10-25 18:26:49 +0900

    챕터3 예제 15~17 코드 작성

---
+++ Chapter03/ch03_Ex15/OperatorEx15.java
@@ -0,0 +1,12 @@
+package ch03_Ex15;
+
+public class OperatorEx15 {
+
+	public static void main(String[] args) {
+		
+		char lowerCase = 'a';
+		char pperCase = (char)(lowerCase - 32);
+		
+		System.out.println(pperCase);
+	}
+}
