1b333f6ea5b60b0c20afbae1065dc595a66d5812
DESKTOP-Q00AR0U\LENOVO <yeosepchoi@naver.com>
2021-10-26 15:40:02 +0900

    챕터3 예제 18~23 코드 작성

---
+++ Chapter03/ch03_Ex18/OperatorEx18.java
@@ -0,0 +1,14 @@
+package ch03_Ex18;
+
+public class OperatorEx18 {
+
+	public static void main(String[] args) {
+		
+		double pi = 3.141592;
+		double shortPi = Math.round(pi * 1000);
+		System.out.println(shortPi);
+		
+		shortPi = Math.round(pi * 1000)/1000.0;
+		System.out.println(shortPi);
+	}
+}
