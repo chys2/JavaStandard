afbb0e6c8fd782179a1e3e9b8300dafcca4afcd9
DESKTOP-3VPJV04\user <yeosepchoi@naver.com>
2021-11-19 22:42:19 +0900

    챕터5 예제 17 ~ 18 해석

--- Chapter05/ch05_Ex18/ArrayEx18.java
+++ Chapter05/ch05_Ex18/ArrayEx18.java
@@ -4,7 +4,7 @@ public class ArrayEx18 {
 
 	public static void main(String args[]) {
 		
-		int[][] score= new int[][] {
+		int[][] score= new int[][] {		//score[i][j]
 			{100,100,100},
 			{20,20,20},
 			{30,30,30},
@@ -15,13 +15,13 @@ public class ArrayEx18 {
 		
 		for(int i=0; i<score.length; i++) {
 			for(int j=0; j<score[i].length; j++) {
-				System.out.printf("score[%d][%d] = %d%n",i,j,score[i][j]);
+				System.out.printf("score[%d][%d] = %d%n",i,j,score[i][j]);		//행과 열에 해당하는 숫자 출력
 			}
 			
 		}
 		
-		for(int[] tmp:score) {
-			for(int i : tmp) {
+		for(int[] tmp:score) {		//score의 각 요소(1차원 배열 주소)를 tmp에 저장
+			for(int i : tmp) {		//tmp는 1차원 배열을 가르키는 참조변수
 				sum += i;
 			}
 		}
